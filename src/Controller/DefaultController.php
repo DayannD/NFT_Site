520945d8b115c583b7bfe20a6cd2ad395211927f
Dayann <dayanndeshayes@gmail.com>
2022-01-14 23:00:07 +0100

    Récupération des données de l'API

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\CallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default')]
-    public function index(): Response
+    public function index(CallApiService $DataApi): Response
     {
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'data' => $DataApi->getOpenseaData(),
         ]);
     }
 }
